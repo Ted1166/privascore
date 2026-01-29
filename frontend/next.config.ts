9b4c6e53eeb51df866ad634409331b86dc9c5a87
Ted1166 <ogolated00@gmail.com>
2026-01-29 04:07:21 +0300

    Json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  
+  turbopack: {
+  },
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      };
+    }
+    
+    config.externals = [...config.externals, { bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" }];
+    
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_IAPP_ADDRESS: process.env.NEXT_PUBLIC_IAPP_ADDRESS,
+  },
 };
 
 export default nextConfig;
